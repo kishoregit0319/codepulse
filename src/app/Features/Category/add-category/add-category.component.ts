ca3e4b89f8e69198bdbd774cb2738b5568a5bb2b
Kishore Git R <settyupskilling.gamil.com>
2024-04-10 01:42:20 +0530

    initial commit

---
+++ src/app/Features/Category/add-category/add-category.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnDestroy } from '@angular/core';
+import {AddCategoryRequest} from "../models/add-category-request.model"
+import { CategoryService } from '../category.service';
+import { Subscription } from 'rxjs';
+@Component({
+  selector: 'app-add-category',
+  templateUrl: './add-category.component.html',
+  styleUrls: ['./add-category.component.css']
+})
+export class AddCategoryComponent implements OnDestroy{
+model:AddCategoryRequest;
+private addCategorySubscribe?:Subscription;
+constructor(private categoryService:CategoryService){
+  this.model = {
+    name:'',
+    urlHandle:''
+  }
+}
+  ngOnDestroy(): void {
+     this.addCategorySubscribe?.unsubscribe();
+  }
+  onFromSubmit(){
+    this.addCategorySubscribe = this.categoryService.addCategory(this.model)
+    .subscribe({next:(respose)=>{
+      console.log("Record Saved");
+    },
+    error:()=>{
+      console.log("Error")
+    },
+    complete:()=>{
+      console.log("Completed.")
+    }
+  })
+  }
+}
